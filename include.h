1d9e7d80f7eecc731e477b9383bdd4cdd78799ce
liamebre <164887882+liamebre@users.noreply.github.com>
2025-09-16 13:42:49 -0400

    Create include.h

---
+++ include.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Audio.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "player.h"	
+#include "ground.h"	
+#include "coin.h"
+
+using namespace sf;
+using namespace std;
