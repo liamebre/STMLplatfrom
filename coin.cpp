af1c4f13ea1f3605d8d87949ae1b8cc6b960bf0f
liamebre <164887882+liamebre@users.noreply.github.com>
2025-09-16 13:42:07 -0400

    Create coin.cpp

---
+++ coin.cpp
@@ -0,0 +1,26 @@
+#include "coin.h"
+
+coin::coin()
+{
+	position = { 0,0 };
+	size = 25.0f;
+}
+
+coin::coin(Vector2f position_) {
+	position = position_;
+	size = 25.0f;
+	shape.setRadius(size);
+	shape.setFillColor(Color::Red);
+	shape.setOrigin(shape.getGeometricCenter());
+	shape.setPosition(position);
+}
+
+void coin::collision(player& p)
+{
+	Vector2f distance = { p.position - position };
+	if (abs(distance.x) <= p.size + size && abs(distance.y) <= p.size +size) {
+		p.score += 1; 
+		position = { 0,-100 };
+		shape.setPosition(position);	
+	}
+}
