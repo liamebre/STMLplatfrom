c5e17836bd6d3f462672740279bd8ee252aa55dc
liamebre <164887882+liamebre@users.noreply.github.com>
2025-09-22 13:24:18 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,34 @@ int main() {
 	Vector2f screenSizeF = Vector2f(1920, 1080);
 	VideoMode vm(screenSize);
 	RenderWindow window(vm, "Plat ", State::Fullscreen);	
-	
-	player p1(screenSizeF/2.0f,50);
+		
+
+	vector<ground > grounds;
+	vector<coin> coins;
+	vector<enemy> enemies;
+
+	player p1(screenSizeF/2.0f,50,500);
+
 	ground g1({ screenSizeF.x / 2.0f, 1040 }, { 1920,80 });
+	grounds.push_back(g1);
+
 	ground g2({ screenSizeF.x / 4.0f, 900}, { 400,280 });
+	grounds.push_back(g2);
+
+	ground g3({ screenSizeF.x / 8.0f *11, 1040  }, { 1000,80 });
+	grounds.push_back(g3);
+
+	ground g4({ screenSizeF.x / 2.0f * 3 + 550, 550 }, { 500,80 });
+	grounds.push_back(g4);
+
+	enemy e1( {g3.position.x, 100 }, -400, 400, 225);
+	enemies.push_back(e1);
+
 	coin c1({ screenSizeF.x / 4.0f*3, 900 });
+	coins.push_back(c1);
 
 	Clock clock;
+	Clock gameTimer; // Separate timer for damage system
 
 	Font font;
 	if (!font.openFromFile("fonts/tuffy.ttf")) {
@@ -24,6 +45,9 @@ int main() {
 	Text scoreText(font, "score : 0", 50U);
 	scoreText.setPosition({ 0.0f, 0.0f });
 
+	Text livesText(font, "lives : 3", 50U);
+	livesText.setPosition({200.0f, 0.0f });
+
 	while (window.isOpen()) {
 		while (const optional event = window.pollEvent())
 		{
@@ -31,7 +55,8 @@ int main() {
 				// Quit the game when the window is closed
 				window.close();
 		}
-		Time dt = clock.restart();	
+		Time tt = gameTimer.getElapsedTime(); // Use separate timer that doesn't reset
+		Time dt = clock.restart();
 
 		if (Keyboard::isKeyPressed(Keyboard::Key::D)) {
 			p1.moveRight();
@@ -43,36 +68,99 @@ int main() {
 			p1.moveUp();
 		}
 
-		if (p1.position.x > screenSizeF.x / 10.0f * 8.0f) {
-			g1.position.x -= 0.2;
-			g2.position.x -= 0.2;
+		if (p1.position.x > screenSizeF.x / 10.0f * 7.5f) {
 			p1.position.x -= 0.2;
+			for (int i = 0; i < grounds.size(); i++) {
+				grounds[i].position.x -= 0.2;
+			}
+			for( int i = 0; i < coins.size(); i++) {
+				coins[i].position.x -= 0.2;
+			}
+			for (int i = 0; i < enemies.size(); i++) {
+				enemies[i].position.x -= 0.2;
+				enemies[i].startPos.x -= 0.2; // Also update start position for bounds
+			}
 		}
 
-		if (p1.position.x < screenSizeF.x / 10.0f * 2.0f) {
-			g1.position.x += 0.2;
-			g2.position.x += 0.2;
+		if (p1.position.x < screenSizeF.x / 10.0f * 2.5f) {
 			p1.position.x += 0.2;
+			for (int i = 0; i < grounds.size(); i++) {
+				grounds[i].position.x += 0.2;
+			}
+			for (int i = 0; i < coins.size(); i++) {
+				coins[i].position.x += 0.2;
+			}
+			for (int i = 0; i < enemies.size(); i++) {
+				enemies[i].position.x += 0.2;
+				enemies[i].startPos.x += 0.2; // Also update start position for bounds
+			}
 		}
 		string temp = "score :" + to_string(p1.score);
 		scoreText.setString(temp);
 		scoreText.setPosition({ 0.0f, 0.0f });
 
+		string temp2 = "lives :" + to_string(p1.lives);
+		livesText.setString(temp2);
+		livesText.setPosition({ 200.0f, 0.0f });
+
 
 		p1.update(dt);
-		g1.update(dt);
-		g2.update(dt);
+
+		for (int i = 0; i < grounds.size() ; i++) {
+			grounds[i].update(dt);
+		}
+
+		for (int i = 0; i < coins.size() ; i++) {
+			coins[i].update(dt);
+		}
+
+		for (int i = 0; i < enemies.size(); i++) {
+			enemies[i].update(dt);
+		}
+
+
 		p1.physicsUpdate(dt);
-		g1.checkCollision(p1);
-		g2.checkCollision(p1);
-		c1.collision(p1);
+
+		for (int i = 0; i < enemies.size(); i++) {
+			enemies[i].physicsUpdate(dt);
+		}
+
+
+		for (int i = 0; i < grounds.size(); i++) {
+			grounds[i].checkCollision(p1);
+			for (int j = 0; j < enemies.size(); j++) {
+				grounds[i].checkEnemyCollision(enemies[j]);
+			}
+		}
+
+		for (int i = 0; i < enemies.size(); i++) {
+			enemies[i].checkCollision(p1,tt);
+		}
+
+		for (int i = 0; i < coins.size(); i++) {
+			coins[i].checkCollision(p1);
+		}
 
 		window.clear();
+
 		window.draw(p1.shape);
-		window.draw(g1.shape);
-		window.draw(g2.shape);
-		window.draw(c1.shape);
+
+		for (int i = 0; i < grounds.size(); i++) {
+			window.draw(grounds[i].shape);
+		}
+
+		for (int i = 0; i < coins.size(); i++) {
+			window.draw(coins[i].shape);
+		}
+		
+		for (int i = 0; i < enemies.size(); i++) {
+			window.draw(enemies[i].shape);
+		}
+
 		window.draw(scoreText);
+		window.draw(livesText);
+
+
 		window.display();
 	}
 	return 0;
